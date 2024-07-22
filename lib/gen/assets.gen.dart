a943bfc810fe3f735473a4d1c0ecc3275ffc4f9f
fandi <fandigit7@gmail.com>
2024-07-22 09:22:24 +0700

    menambahkan assets ke pubspec.yaml dan generate assets path

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,338 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/arrow_right.svg
+  String get arrowRight => 'assets/icons/arrow_right.svg';
+
+  /// File path: assets/icons/bayar_angsuran.svg
+  String get bayarAngsuran => 'assets/icons/bayar_angsuran.svg';
+
+  /// File path: assets/icons/booking_fee.svg
+  String get bookingFee => 'assets/icons/booking_fee.svg';
+
+  /// File path: assets/icons/calendar.svg
+  String get calendar => 'assets/icons/calendar.svg';
+
+  /// File path: assets/icons/cd.svg
+  String get cd => 'assets/icons/cd.svg';
+
+  /// File path: assets/icons/checklist.svg
+  String get checklist => 'assets/icons/checklist.svg';
+
+  /// File path: assets/icons/daftar_dokumen.svg
+  String get daftarDokumen => 'assets/icons/daftar_dokumen.svg';
+
+  /// File path: assets/icons/daftar_komplain.svg
+  String get daftarKomplain => 'assets/icons/daftar_komplain.svg';
+
+  /// File path: assets/icons/date.svg
+  String get date => 'assets/icons/date.svg';
+
+  /// File path: assets/icons/dollar.svg
+  String get dollar => 'assets/icons/dollar.svg';
+
+  /// File path: assets/icons/half_clock.svg
+  String get halfClock => 'assets/icons/half_clock.svg';
+
+  /// File path: assets/icons/home.svg
+  String get home => 'assets/icons/home.svg';
+
+  /// File path: assets/icons/location.svg
+  String get location => 'assets/icons/location.svg';
+
+  /// Directory path: assets/icons/navbar
+  $AssetsIconsNavbarGen get navbar => const $AssetsIconsNavbarGen();
+
+  /// File path: assets/icons/notification.svg
+  String get notification => 'assets/icons/notification.svg';
+
+  /// File path: assets/icons/pesanan_belum_bayar.svg
+  String get pesananBelumBayar => 'assets/icons/pesanan_belum_bayar.svg';
+
+  /// File path: assets/icons/riwayat_pesanan.svg
+  String get riwayatPesanan => 'assets/icons/riwayat_pesanan.svg';
+
+  /// File path: assets/icons/search.svg
+  String get search => 'assets/icons/search.svg';
+
+  /// File path: assets/icons/tahap_akad.svg
+  String get tahapAkad => 'assets/icons/tahap_akad.svg';
+
+  /// File path: assets/icons/tahap_ppjb.svg
+  String get tahapPpjb => 'assets/icons/tahap_ppjb.svg';
+
+  /// File path: assets/icons/tahap_serah_terima_bangunan.svg
+  String get tahapSerahTerimaBangunan =>
+      'assets/icons/tahap_serah_terima_bangunan.svg';
+
+  /// File path: assets/icons/tahap_serah_terima_legalitas.svg
+  String get tahapSerahTerimaLegalitas =>
+      'assets/icons/tahap_serah_terima_legalitas.svg';
+
+  /// File path: assets/icons/tahap_sp3k.svg
+  String get tahapSp3k => 'assets/icons/tahap_sp3k.svg';
+
+  /// File path: assets/icons/tahap_spr.svg
+  String get tahapSpr => 'assets/icons/tahap_spr.svg';
+
+  /// File path: assets/icons/tahap_sps.svg
+  String get tahapSps => 'assets/icons/tahap_sps.svg';
+
+  /// File path: assets/icons/twotoon_category.svg
+  String get twotoonCategory => 'assets/icons/twotoon_category.svg';
+
+  /// File path: assets/icons/warning.svg
+  String get warning => 'assets/icons/warning.svg';
+
+  /// List of all assets
+  List<String> get values => [
+        arrowRight,
+        bayarAngsuran,
+        bookingFee,
+        calendar,
+        cd,
+        checklist,
+        daftarDokumen,
+        daftarKomplain,
+        date,
+        dollar,
+        halfClock,
+        home,
+        location,
+        notification,
+        pesananBelumBayar,
+        riwayatPesanan,
+        search,
+        tahapAkad,
+        tahapPpjb,
+        tahapSerahTerimaBangunan,
+        tahapSerahTerimaLegalitas,
+        tahapSp3k,
+        tahapSpr,
+        tahapSps,
+        twotoonCategory,
+        warning
+      ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/american_house.png
+  AssetGenImage get americanHouse =>
+      const AssetGenImage('assets/images/american_house.png');
+
+  /// File path: assets/images/background_empty.png
+  AssetGenImage get backgroundEmpty =>
+      const AssetGenImage('assets/images/background_empty.png');
+
+  /// File path: assets/images/bukit_raya_residance.png
+  AssetGenImage get bukitRayaResidance =>
+      const AssetGenImage('assets/images/bukit_raya_residance.png');
+
+  /// File path: assets/images/bukit_tinggi.png
+  AssetGenImage get bukitTinggi =>
+      const AssetGenImage('assets/images/bukit_tinggi.png');
+
+  /// File path: assets/images/candra_bhirawa.jpg
+  AssetGenImage get candraBhirawa =>
+      const AssetGenImage('assets/images/candra_bhirawa.jpg');
+
+  /// File path: assets/images/profile_avatar.png
+  AssetGenImage get profileAvatar =>
+      const AssetGenImage('assets/images/profile_avatar.png');
+
+  /// File path: assets/images/splash_screen.png
+  AssetGenImage get splashScreen =>
+      const AssetGenImage('assets/images/splash_screen.png');
+
+  /// File path: assets/images/splash_screen_12.png
+  AssetGenImage get splashScreen12 =>
+      const AssetGenImage('assets/images/splash_screen_12.png');
+
+  /// File path: assets/images/tahap_administrasi.png
+  AssetGenImage get tahapAdministrasi =>
+      const AssetGenImage('assets/images/tahap_administrasi.png');
+
+  /// File path: assets/images/tahap_akad_serah_terima.png
+  AssetGenImage get tahapAkadSerahTerima =>
+      const AssetGenImage('assets/images/tahap_akad_serah_terima.png');
+
+  /// File path: assets/images/tahap_pembangunan.png
+  AssetGenImage get tahapPembangunan =>
+      const AssetGenImage('assets/images/tahap_pembangunan.png');
+
+  /// File path: assets/images/tahap_pemesanan.png
+  AssetGenImage get tahapPemesanan =>
+      const AssetGenImage('assets/images/tahap_pemesanan.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        americanHouse,
+        backgroundEmpty,
+        bukitRayaResidance,
+        bukitTinggi,
+        candraBhirawa,
+        profileAvatar,
+        splashScreen,
+        splashScreen12,
+        tahapAdministrasi,
+        tahapAkadSerahTerima,
+        tahapPembangunan,
+        tahapPemesanan
+      ];
+}
+
+class $AssetsIconsNavbarGen {
+  const $AssetsIconsNavbarGen();
+
+  /// Directory path: assets/icons/navbar/active
+  $AssetsIconsNavbarActiveGen get active => const $AssetsIconsNavbarActiveGen();
+
+  /// Directory path: assets/icons/navbar/nonactive
+  $AssetsIconsNavbarNonactiveGen get nonactive =>
+      const $AssetsIconsNavbarNonactiveGen();
+}
+
+class $AssetsIconsNavbarActiveGen {
+  const $AssetsIconsNavbarActiveGen();
+
+  /// File path: assets/icons/navbar/active/account.svg
+  String get account => 'assets/icons/navbar/active/account.svg';
+
+  /// File path: assets/icons/navbar/active/building.svg
+  String get building => 'assets/icons/navbar/active/building.svg';
+
+  /// File path: assets/icons/navbar/active/cart.svg
+  String get cart => 'assets/icons/navbar/active/cart.svg';
+
+  /// File path: assets/icons/navbar/active/document.svg
+  String get document => 'assets/icons/navbar/active/document.svg';
+
+  /// File path: assets/icons/navbar/active/home.svg
+  String get home => 'assets/icons/navbar/active/home.svg';
+
+  /// List of all assets
+  List<String> get values => [account, building, cart, document, home];
+}
+
+class $AssetsIconsNavbarNonactiveGen {
+  const $AssetsIconsNavbarNonactiveGen();
+
+  /// File path: assets/icons/navbar/nonactive/account.svg
+  String get account => 'assets/icons/navbar/nonactive/account.svg';
+
+  /// File path: assets/icons/navbar/nonactive/building.svg
+  String get building => 'assets/icons/navbar/nonactive/building.svg';
+
+  /// File path: assets/icons/navbar/nonactive/cart.svg
+  String get cart => 'assets/icons/navbar/nonactive/cart.svg';
+
+  /// File path: assets/icons/navbar/nonactive/document.svg
+  String get document => 'assets/icons/navbar/nonactive/document.svg';
+
+  /// File path: assets/icons/navbar/nonactive/home.svg
+  String get home => 'assets/icons/navbar/nonactive/home.svg';
+
+  /// List of all assets
+  List<String> get values => [account, building, cart, document, home];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
